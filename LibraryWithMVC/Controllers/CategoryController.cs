ce09a3357ed391c89dc752c08dd573c7c50837ad
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-02 22:09:08 +0300

    Lecture 13 - Kategori Ekleme

--- LibraryWithMVC/Controllers/CategoryController.cs
+++ LibraryWithMVC/Controllers/CategoryController.cs
@@ -13,7 +13,24 @@ namespace LibraryWithMVC.Controllers
             {
                 return View(db.tbl_category.ToList());
             }
-
         }
+
+        // GET: AddCategory
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(tbl_category category)
+        {
+            using (DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities())
+            {
+                db.tbl_category.Add(category);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
