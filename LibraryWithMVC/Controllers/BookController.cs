6c6769c9ae8c8ea9af02e78e0240bc43398454f8
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-03 15:01:02 +0300

    Lecture 24 - Yeni Kitap Ekleme Sayfa Tasarımı

--- LibraryWithMVC/Controllers/BookController.cs
+++ LibraryWithMVC/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryWithMVC.Models.Entity;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Mvc;
 
 namespace LibraryWithMVC.Controllers
@@ -10,8 +11,22 @@ namespace LibraryWithMVC.Controllers
         // GET: Book
         public ActionResult Index()
         {
+            return View(db.tbl_book.ToList());
+        }
 
-                return View(db.tbl_book.ToList());
+        // GET:AddBook
+        [HttpGet]
+        public ActionResult AddBook()
+        {
+            //SelectList books = db.tbl_book.Select(x => x.tbl_category.ctg_id == x.bk_ctg,);
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddBook(tbl_book book)
+        {
+            return View();
         }
     }
 }
