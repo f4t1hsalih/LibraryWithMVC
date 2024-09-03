6661316db133015ccd8e6179ded569ce28526a80
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-03 18:00:14 +0300

    Lecture 35 - Üye Listeleme

---
+++ LibraryWithMVC/Controllers/MemberController.cs
@@ -0,0 +1,22 @@
+﻿using LibraryWithMVC.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryWithMVC.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Index()
+        {
+            using (DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities())
+            {
+                var member = db.tbl_member.ToList();
+                return View(member);
+            }
+        }
+    }
+}
