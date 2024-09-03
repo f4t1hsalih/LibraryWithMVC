0416ad518d85acf8d70cbf37170ed3a22266d81b
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-03 14:42:02 +0300

    Lecture 23 - İlişkili Tablolar ile Kitap Listesi

---
+++ LibraryWithMVC/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using LibraryWithMVC.Models.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LibraryWithMVC.Controllers
+{
+    public class BookController : Controller
+    {
+        DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities();
+        // GET: Book
+        public ActionResult Index()
+        {
+
+                return View(db.tbl_book.ToList());
+        }
+    }
+}
