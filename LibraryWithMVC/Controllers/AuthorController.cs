da2204cd262f78c7935693c210feacd16301288b
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-03 11:43:03 +0300

    Lecture 18 - Yazar Listesi

---
+++ LibraryWithMVC/Controllers/AuthorController.cs
@@ -0,0 +1,19 @@
+﻿using LibraryWithMVC.Models.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LibraryWithMVC.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        public ActionResult Index()
+        {
+            using (DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities())
+            {
+                return View(db.tbl_author.ToList());
+            }
+
+        }
+    }
+}
