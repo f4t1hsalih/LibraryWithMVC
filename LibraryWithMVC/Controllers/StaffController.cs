d7f2bfa18dcec3a8581291bffc731cdbb10f73d4
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-03 17:13:10 +0300

    Lecture 32 - Personel Listeleme

---
+++ LibraryWithMVC/Controllers/StaffController.cs
@@ -0,0 +1,19 @@
+﻿using LibraryWithMVC.Models.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LibraryWithMVC.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            using (DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities())
+            {
+                var staff = db.tbl_staff.ToList();
+                return View(staff);
+            }
+        }
+    }
+}
