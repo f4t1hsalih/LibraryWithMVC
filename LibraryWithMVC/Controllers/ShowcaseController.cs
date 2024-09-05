c0f0c48106e12f141946bb489f3dd4a8cd294ad0
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-05 17:37:06 +0300

    Lecture 57 - Tema Düzenleme

--- LibraryWithMVC/Controllers/ShowcaseController.cs
+++ LibraryWithMVC/Controllers/ShowcaseController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace LibraryWithMVC.Controllers
 {
@@ -11,6 +7,7 @@ namespace LibraryWithMVC.Controllers
         // GET: Showcase
         public ActionResult Index()
         {
+            ViewBag.Title = "Vitrin Paneli";
             return View();
         }
     }
