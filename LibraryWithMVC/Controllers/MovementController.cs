82c6c12479b5086bc913d538e056e44b7ed7a136
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-05 16:55:48 +0300

    Lecture 54-55 - İşlemler Sayfası

---
+++ LibraryWithMVC/Controllers/MovementController.cs
@@ -0,0 +1,19 @@
+﻿using LibraryWithMVC.Models.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LibraryWithMVC.Controllers
+{
+    public class MovementController : Controller
+    {
+        // GET: Movement
+        public ActionResult Index()
+        {
+            DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities();
+
+            var values = db.tbl_movement.ToList();
+            return View(values);
+
+        }
+    }
+}
