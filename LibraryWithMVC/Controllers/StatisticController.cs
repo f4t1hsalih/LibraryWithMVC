bcc1911f8ca0d41e2425ed986d206c86847559e0
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-09 11:37:09 +0300

    Lecture 83 - Linq İle Gruplama Sorguları

--- LibraryWithMVC/Controllers/StatisticController.cs
+++ LibraryWithMVC/Controllers/StatisticController.cs
@@ -54,8 +54,62 @@ namespace LibraryWithMVC.Controllers
                 ViewBag.safe = db.tbl_punishment.Sum(x => x.pnh_money);
                 ViewBag.bookInDeposit = db.tbl_book.Where(x => x.bk_status == false).Count();
                 ViewBag.CategoryCount = db.tbl_category.Count();
+                ViewBag.MostActiveMember = db.tbl_member
+                    .Where(m => m.mmb_id == db.tbl_movement
+                        .GroupBy(mv => mv.mvm_mmb)  // Üye ID'sine göre gruplamar
+                        .Select(g => new
+                        {
+                            MemberId = g.Key,
+                            TotalBooks = g.Count()  // Toplam kitap hareketi sayısını hesaplar
+                        })
+                        .OrderByDescending(g => g.TotalBooks)  // En fazla kitap hareketi yapan üyeyi seçer
+                        .Select(g => g.MemberId)
+                        .FirstOrDefault())
+                    .Select(m => m.mmb_name + " " + m.mmb_surname)
+                    .FirstOrDefault();
+                ViewBag.MostReadBook = db.tbl_book
+                    .Where(b => b.bk_id == db.tbl_movement
+                        .GroupBy(m => m.mvm_bk)  // Kitap ID'sine göre gruplamar
+                        .Select(g => new
+                        {
+                            BookId = g.Key,
+                            TotalReads = g.Count()  // Toplam okuma sayısını hesaplar
+                        })
+                        .OrderByDescending(g => g.TotalReads)  // En fazla okunan kitabı seçer
+                        .Select(g => g.BookId)
+                        .FirstOrDefault())
+                    .Select(b => b.bk_name)  // Kitap adını seçer
+                    .FirstOrDefault();
                 ViewBag.TotalMessage = db.tbl_communication.Count();
-                ViewBag.GetTopAuthorByBookCount = db.GetTopAuthorByBookCount().FirstOrDefault().Yazar;
+                ViewBag.PublisherWithMostBooks = db.tbl_book.GroupBy(x => x.bk_publishing_house).OrderByDescending(x => x.Count()).Select(x => x.Key).FirstOrDefault();
+                ViewBag.MostSuccessfulStaff = db.tbl_staff
+                    .Where(s => s.stf_id == db.tbl_movement
+                        .GroupBy(m => m.mvm_stf)  // Personel ID'sine göre gruplamar
+                        .Select(g => new
+                        {
+                            StaffId = g.Key,
+                            TotalMovements = g.Count()  // Toplam hareket sayısını hesaplar
+                        })
+                        .OrderByDescending(g => g.TotalMovements)  // En fazla hareket yapan personeli seçer
+                        .Select(g => g.StaffId)
+                        .FirstOrDefault())
+                    .Select(s => s.stf_name + " " + s.stf_surname)  // Personel ismini seçer
+                    .FirstOrDefault();
+                ViewBag.CategoryWithMostBooks = db.tbl_category
+                    .Where(c => c.ctg_id == db.tbl_book
+                        .GroupBy(b => b.bk_ctg)  // Kategori ID'sine göre gruplamar
+                        .Select(g => new
+                        {
+                            CategoryId = g.Key,
+                            TotalBooks = g.Count()  // Toplam kitap sayısını hesaplar
+                        })
+                        .OrderByDescending(g => g.TotalBooks)  // En fazla kitaba sahip kategoriyi seçer
+                        .Select(g => g.CategoryId)
+                        .FirstOrDefault())
+                    .Select(c => c.ctg_name)  // Kategori adını seçer
+                    .FirstOrDefault();
+                ViewBag.TopAuthorByBookCount = db.GetTopAuthorByBookCount().FirstOrDefault().Yazar;
+
                 return View();
             }
         }
