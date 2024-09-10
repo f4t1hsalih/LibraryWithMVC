4324989f6b9eb7f7da8e33c110255dffb9fad461
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-10 14:21:18 +0300

    Lecture 84 - Login Controller

---
+++ LibraryWithMVC/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace LibraryWithMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
