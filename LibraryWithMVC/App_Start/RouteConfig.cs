a3b4b645b692c860a074a85d6b6599ef3e78200c
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-11 17:44:51 +0300

    Lecture 89 - Öğrenci Paneli Template Kurulumu

--- LibraryWithMVC/App_Start/RouteConfig.cs
+++ LibraryWithMVC/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace LibraryWithMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Showcase", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
