2187e183fdacd669fc3c3ec496cf6a76680cfafa
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-11 18:18:58 +0300

    Lecture 92 - Login İşlemi Giriş Yapma

--- LibraryWithMVC/Controllers/LoginController.cs
+++ LibraryWithMVC/Controllers/LoginController.cs
@@ -1,14 +1,32 @@
 ﻿using LibraryWithMVC.Models.Entity;
+using System.Linq;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace LibraryWithMVC.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(tbl_member member)
+        {
+            using (DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities())
+            {
+                tbl_member mmb = db.tbl_member.FirstOrDefault(x => x.mmb_username == member.mmb_username && x.mmb_password == member.mmb_password);
+                if (mmb != null)
+                {
+                    FormsAuthentication.SetAuthCookie(mmb.mmb_name + " " + mmb.mmb_surname, false);
+                    Session.Add("id", mmb.mmb_id);
+                    return RedirectToAction("Index","StudentPanel");
+                }
+                else { return View(member); }
+            }
         }
 
         [HttpGet]
