3a64304f16c99740372fa0d92fa845ae27417a7d
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-11 21:42:35 +0300

    Lecture 100 - Profil Bilgileri Güncelleme

--- LibraryWithMVC/Controllers/StudentPanelController.cs
+++ LibraryWithMVC/Controllers/StudentPanelController.cs
@@ -14,10 +14,43 @@ namespace LibraryWithMVC.Controllers
             using (DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities())
             {
                 int id = Convert.ToInt32(Session["id"].ToString());
-                var value = db.tbl_member.FirstOrDefault(x => x.mmb_id == id);
+                tbl_member value = db.tbl_member.FirstOrDefault(x => x.mmb_id == id);
                 ViewBag.fullname = value.mmb_name + " " + value.mmb_surname;
                 return View(value);
             }
+        }
+
+        [HttpGet]
+        public ActionResult EditProfile(int id)
+        {
+            using (DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities())
+            {
+                var values = db.tbl_member.FirstOrDefault(x => x.mmb_id == id);
+                return View(values);
+            }
+        }
+        [HttpPost]
+        public ActionResult EditProfile(tbl_member mem)
+        {
+            using (DB_LibraryWithMVCEntities db = new DB_LibraryWithMVCEntities())
+            {
+                tbl_member value = db.tbl_member.FirstOrDefault(x => x.mmb_id == mem.mmb_id);
+                value.mmb_name = mem.mmb_name;
+                value.mmb_surname = mem.mmb_surname;
+                value.mmb_email = mem.mmb_email;
+                value.mmb_tel = mem.mmb_tel;
+                value.mmb_photo = mem.mmb_photo;
+                value.mmb_school = mem.mmb_school;
+                value.mmb_password = mem.mmb_password;
+                db.Entry(value).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return View("Index",value);
+            }
+        }
+        public ActionResult Exit()
+        {
+            Session.Clear();
+            return RedirectToAction("Index","Showcase");
         }
     }
 }
