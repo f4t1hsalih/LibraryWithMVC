fda4271e3fe21a72e0d0322a823a6fc7c0869c9a
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-12 14:25:08 +0300

    Lecture 101 - Mesajlar Sayfasının Oluşturulması

---
+++ LibraryWithMVC/Controllers/MessageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryWithMVC.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewMessage()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
