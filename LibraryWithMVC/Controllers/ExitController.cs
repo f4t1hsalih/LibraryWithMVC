feedb870d6ee6993d4bd956f151a49741f006d05
f4t1hsalih <f4t1hsalih@gmail.com>
2024-09-14 17:59:30 +0300

    Tasarım Değişiklikleri Yapıldı

---
+++ LibraryWithMVC/Controllers/ExitController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace LibraryWithMVC.Controllers
+{
+    public class ExitController : Controller
+    {
+        // GET: Exit
+        public ActionResult Index()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index", "Showcase");
+        }
+    }
+}
